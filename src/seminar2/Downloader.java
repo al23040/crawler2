c86887ce5b8d762c03d1f9c80adb56ce0502652e
al23040 <al23040@shibaura-it.ac.jp>
2026-02-16 17:38:49 +0900

    ダウンロード方法の統一

--- src/seminar2/Downloader.java
+++ src/seminar2/Downloader.java
@@ -1,11 +1,9 @@
 package seminar2;
 
 import java.io.BufferedInputStream;
-import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.HttpURLConnection;
-import java.net.URI;
 import java.net.URL;
 import java.nio.file.Files;
 import java.nio.file.Paths;
@@ -18,10 +16,9 @@ public class Downloader {
 	public Downloader(String baseDir) {
 		this.baseDir = baseDir;
 	}
+	
 	public String downloadImage(String urlStr, String path) {
 		HttpURLConnection connection = null;
-		InputStream in = null;
-		FileOutputStream out = null;
 		
 		try {
 			URL url = new URL(urlStr);
@@ -39,42 +36,26 @@ public class Downloader {
             
             String filePath = baseDir + path + getExtension(contentType);
             String htmlPath = path + getExtension(contentType);
-            
-            in = new BufferedInputStream(connection.getInputStream());
-            out = new FileOutputStream(filePath);
-            
-            byte[] buffer = new byte[1024];
-            int bytesRead;
-            while ((bytesRead = in.read(buffer)) != -1) {
-            	out.write(buffer, 0, bytesRead);
-            }
-            
-            System.out.println(filePath + "をダウンロードしました．");
-            
-            out.close();
-            in.close();
-            connection.disconnect();
-            
+            download(connection, filePath);
             return htmlPath;
 		} catch (IOException e) {
         	e.printStackTrace();
         	return null;
         }
 	}
-	public void downloadFile(String url, String path) {
+	
+	public void downloadFile(String urlStr, String path) {
 		try {
-            URL targetUrl = URI.create(url).toURL();
-            
-            HttpURLConnection conn = (HttpURLConnection) targetUrl.openConnection();
-            conn.setRequestProperty("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64)");
-
-            try (InputStream in = conn.getInputStream()) {
-                Files.copy(in, Paths.get(path), StandardCopyOption.REPLACE_EXISTING);
-            }
+			URL url = new URL(urlStr);
+			HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+			connection.setRequestProperty("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64)");
+			connection.setConnectTimeout(5000);
+			download(connection, path);
         } catch (Exception e) {
             System.err.println("ダウンロードエラー");
         }
 	}
+	
 	public void downloadHtml(String path, Document doc) {
 		try {
 			Files.write(Paths.get(path), doc.outerHtml().getBytes());
@@ -82,6 +63,16 @@ public class Downloader {
 			e.printStackTrace();
 		}
 	}
+	
+	private void download(HttpURLConnection connection, String path) {
+		try (InputStream in = new BufferedInputStream(connection.getInputStream())) {
+        	Files.copy(in, Paths.get(path), StandardCopyOption.REPLACE_EXISTING);
+        } catch (IOException e) {
+        	System.err.println(path + "のダウンロード失敗");
+        }
+		System.out.println(path + "をダウンロードしました．");
+	}
+	
 	public String getExtension(String contentType) {
 		if (contentType == null) return ".jpg";
 		
